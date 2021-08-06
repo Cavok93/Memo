56d27c7818ca086bad813cb5ff79e460f9e8b382
Cavok93 <fire93sim@naver.com>
2021-08-06 16:48:17 +0900

    추가: memoList Scene 구현

--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
+
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
     
@@ -14,7 +18,13 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     var viewModel: MemoListViewModel!
     
     
-
+    @IBOutlet weak var listTableView: UITableView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +33,18 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "cell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag
+            
+            )
     }
 
 }
--- RxMemo/ViewModel/MemoListViewModel.swift
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -19,4 +19,5 @@ class MemoListViewModel: CommonViewModel {
     
     
     
+    
 }
