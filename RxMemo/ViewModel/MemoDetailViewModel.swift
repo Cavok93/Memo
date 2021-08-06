9a3766ec31b8a2a0fe9c25e0ee594f3487427ac7
Cavok93 <fire93sim@naver.com>
2021-08-06 18:52:04 +0900

    추가: MemoDtailViewModel을 Controller에 바인딩

--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,9 +6,38 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
 
-
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    
+    let memo: Memo
+    
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    
+    
+    //MARK: 편집내용 반영을 위한 새로운 문자열 배열을 방출하는 BehaviorSubject
+    var contents: BehaviorSubject<[String]>
+    
+    
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [memo.content, formatter.string(from: memo.insertDate)])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
+    
+    
     
 }
